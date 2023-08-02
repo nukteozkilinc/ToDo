ac1bc7ce5934e5db628e24ac76f3047709c43152
nukteozkilinc <nukteozkilinc06@gmail.com>
2023-08-02 21:52:49 +0300

    first commit

---
+++ ToDo/data/entity/ToDos.swift
@@ -0,0 +1,8 @@
+//
+//  ToDos.swift
+//  ToDo
+//
+//  Created by Nukte Ozkilinc on 2.08.2023.
+//
+
+import Foundation
---
+++ ToDo/ui/views/UpdateToDo.swift
@@ -0,0 +1,29 @@
+//
+//  UpdateToDo.swift
+//  ToDo
+//
+//  Created by Nukte Ozkilinc on 2.08.2023.
+//
+
+import UIKit
+
+class UpdateToDo: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
